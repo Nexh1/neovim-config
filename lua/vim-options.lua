4ab9f2fc873f9cdfabcd6f06e7aacd8c9dc8e904
Kevin Ponsart <kevinponsart89@gmail.com>
2025-08-16 21:46:24 +0200

    :fix: remove useless comments

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -14,12 +14,3 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
---vim.keymap.set("n", "<leader>l", function()
---  vim.cmd("wincmd l")
---end, { noremap = true, silent = true })
-
---vim.keymap.set("n", "<leader>h", function()
---  vim.cmd("wincmd h")
---end, { noremap = true, silent = true })
-
