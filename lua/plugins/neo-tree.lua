e4d5aa5f2b7e08e97674d45da74fe5dde68fd667
Kevin Ponsart <kevinponsart89@gmail.com>
2025-08-17 15:09:30 +0200

    :fix: show ignored files in neotree & telescope

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,4 +1,4 @@
-return  {
+return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
   dependencies = {
@@ -8,8 +8,42 @@ return  {
   },
   lazy = false,
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added     = "✚",
+            modified  = "",
+            deleted   = "✖",
+            renamed   = "󰁕",
+            untracked = "★",
+          },
+        },
+      },
+    })
+
     vim.keymap.set('n', '<C-n>', function()
-      require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd(), position = "left", reveal = true })
+      require("neo-tree.command").execute({
+        toggle = true,
+        dir = vim.loop.cwd(),
+        position = "left",
+        reveal = true,
+      })
     end, { noremap = true, silent = true })
+
+    vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { fg = "#e06c75", bold = true })
+    vim.api.nvim_set_hl(0, "NeoTreeGitAdded",     { fg = "#98c379" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitModified",  { fg = "#e5c07b" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitDeleted",   { fg = "#be5046" })
+    vim.api.nvim_set_hl(0, "NeoTreeGitRenamed",   { fg = "#61afef" })
   end
 }
+
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,24 +1,34 @@
 return {
   {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+    },
     config = function()
+      local telescope = require("telescope")
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    end
-  },
-  {
-    'nvim-telescope/telescope-ui-select.nvim',
-    config = function ()
-      require("telescope").setup {
+
+      telescope.setup {
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown {}
           }
-        }
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+          }
+        },
       }
-      require("telescope").load_extension("ui-select")
+
+      telescope.load_extension("ui-select")
+
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
-  },
-} 
+  }
+}
+
